940f92bbad4d9df3fdc76df0b470273b8b03c74c
DrMachoo <60426203+DrMachoo@users.noreply.github.com>
2020-03-15 16:47:55 +0100

    Update Sprites.py

--- venv/Sprites.py
+++ venv/Sprites.py
@@ -1,8 +1,13 @@
 import pygame as pg
-
+#De sprites som vi bruger til baggrunden og som laver animationen for charakteren
+#Vores karakter har ikke en animation lige nu fordi at vi skulle ændre i koden for at få ham til at skyde
+#Forhåbenligt kan vi fikse det senere
 walkRight = [pg.image.load('R1.png'), pg.image.load('R2.png'), pg.image.load('R3.png'), pg.image.load('R4.png'), pg.image.load('R5.png'), pg.image.load('R6.png'), pg.image.load('R7.png'), pg.image.load('R8.png'), pg.image.load('R9.png')]
 walkLeft = [pg.image.load('L1.png'), pg.image.load('L2.png'), pg.image.load('L3.png'), pg.image.load('L4.png'), pg.image.load('L5.png'), pg.image.load('L6.png'), pg.image.load('L7.png'), pg.image.load('L8.png'), pg.image.load('L9.png')]
 #walkUp
 #walkDown
 bg = pg.image.load('Bg.png')
 char = pg.image.load('standing.png')
+
+
+
