7c9695ee41bc57b4db9dceb988af0d219f2386b0
Bauer <will1717@edu.aarhustech.dk>
2020-04-15 13:08:37 +0200

    Noice

--- Settings.py
+++ Settings.py
@@ -53,7 +53,7 @@ class Player(object):
             screen.blit(char, (self.x,self.y))
         #Så vi kan se vores hitbox
         self.hitbox = (self.x + 17, self.y + 11, 31, 57)
-        pg.draw.rect(screen, (255,0,0), self.hitbox, 2)
+        #pg.draw.rect(screen, (255,0,0), self.hitbox, 2)
         #Hp
         pg.draw.rect(screen, (255, 0, 0), (self.hitbox[0] - 10, self.hitbox[1] - 20, 50, 10))
         pg.draw.rect(screen, (0, 128, 0), (self.hitbox[0] - 10, self.hitbox[1] - 20, 50 - ((50 / 4) * (3 - self.health)), 10))
@@ -97,14 +97,6 @@ class projectile(object):
 #Vi er i gang med at lave en enemy class som så vil følge efter dig
 
 
-
-
-
-
-
-
-
-
 class Enemy(object):
     def __init__(self, x, y , Ewidth, Eheight):
         self.x = x
@@ -123,7 +115,7 @@ class Enemy(object):
             screen.blit(char_enemy, (self.x, self.y))
             #pg.draw.rect(screen, Black, (self.x, self.y, self.Ewidth, self.Eheight))
             self.hitbox = (self.x + 17, self.y + 11, 31, 57)
-            pg.draw.rect(screen, (255, 0, 0), self.hitbox, 2)
+            #pg.draw.rect(screen, (255, 0, 0), self.hitbox, 2)
             #Healthbar
             pg.draw.rect(screen, (255,0,0), (self.hitbox[0] - 10, self.hitbox[1] - 20, 50, 10))
             pg.draw.rect(screen, (0,128, 0), (self.hitbox[0] - 10, self.hitbox[1] - 20, 50 - ((50 / 4) * (3 - self.health)), 10))
