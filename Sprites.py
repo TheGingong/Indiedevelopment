a118c23198de56b05a77b45aca6390b7f1a68ba2
DrMachoo <60426203+DrMachoo@users.noreply.github.com>
2020-05-27 13:53:28 +0200

    Update Sprites.py

--- Sprites.py
+++ Sprites.py
@@ -8,16 +8,18 @@ pg.init()
 WalkRight = [pg.image.load('Rightsideside_player.png'), pg.image.load('Rightsideside_player.png'), pg.image.load('Rightsideside_player.png'), pg.image.load('Rightsideside_player.png'), pg.image.load('Rightsideside_player.png'), pg.image.load('Rightsideside_player.png'), pg.image.load('Rightsideside_player.png'), pg.image.load('Rightsideside_player.png'), pg.image.load('Rightsideside_player.png')]
 WalkLeft = [pg.image.load('Leftside_player.png'), pg.image.load('Leftside_player.png'), pg.image.load('Leftside_player.png'), pg.image.load('Leftside_player.png'), pg.image.load('Leftside_player.png'), pg.image.load('Leftside_player.png'), pg.image.load('Leftside_player.png'), pg.image.load('Leftside_player.png'), pg.image.load('Leftside_player.png')]
 
+
 bg = pg.image.load('Arena.png')
-#Menuq = pg.image.load('BowRain_main_menu.jpg')
+#Menu = pg.image.load('BowRain_main_menu.jpg')
 char = pg.image.load('standing_player.png')
 char_enemy = pg.image.load('standing_enemy.png')
-heartSprite = pg.image.load('Heart-health.png')
+heartSprite = pg.image.load('Heart.png')
 GameOver = pg.image.load('GameOver.png')
 
 
 Music = pg.mixer.music.load('SpilMusik.mp3')
-BulletSound = pg.mixer.Sound('bullet.wav')
-HitSound = pg.mixer.Sound('hit.wav')
+BulletSound = pg.mixer.Sound('bullet.mp3')
+HitSound = pg.mixer.Sound('hit.mp3')
+
 
 
